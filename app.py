52ca2d102217acf7d79d56988a7e62f645c0fb05
Nicola2309 <nicola.spigaglia@outlook.it>
2021-02-03 14:40:44 +0000

    Login and Logout functions implemented

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def register():
         # Let the user in their session
         session["user"] = request.form.get("username").lower()
         flash("Welcome, Food Lover!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -63,9 +64,12 @@ def login():
         if existing_user:
             # Stored hashed password must match user's input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+                existing_user["password"], request.form.get(
+                    "password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # if the passwords don't match
                 flash("Incorrect Username and/or Password")
@@ -77,6 +81,26 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # Display Username in session using DB data
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # User session cookies removal
+    flash("See you soon Chef!")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
