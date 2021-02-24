ae305c7af98e44bf74aee4732fd161914d3b4848
Nicola2309 <nicola.spigaglia@outlook.it>
2021-02-24 16:01:48 +0000

    Footer added

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
     $('.sidenav').sidenav({ edge:"right" });
     $('select').formSelect();
     $('.modal').modal();
+    $('.carousel.carousel-slider').carousel(
+    {
+    fullWidth: true,
+    indicators: true,
+  });
+  setInterval(function() {
+    ('.carousel').carousel('next');
+  }, 3000);    
+
 });
