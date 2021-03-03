de3fa0c613da137e2c013dbca36e015072c0bced
Nicola2309 <nicola.spigaglia@outlook.it>
2021-03-03 14:48:08 +0000

    Added smooth scrolling to About Us button

--- static/js/script.js
+++ static/js/script.js
@@ -8,7 +8,59 @@ $(document).ready(function(){
     indicators: true,
   });
   setInterval(function() {
-    ('.carousel').carousel('next');
+    $('.carousel').carousel('next');
   }, 3000);    
 
+});
+
+/* Scrolling navbar code inspired from the tutorial at this link https://www.youtube.com/watch?v=HINs4W_pGGU&ab_channel=ConorBailey */
+
+$(window).scroll(function(){
+
+    $('nav').toggleClass('scrolled', $(this).scrollTop()>42);
+});
+
+// Transition Functions 
+/*function scrollAppear(){
+    let textSlideInit = document.querySelector('.text-1-slide-init');
+    let beginPosition = textSlideInit.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight / 1.5;
+
+    if(beginPosition < screenPosition){
+        textSlideInit.classList.add('.text-1-slide-end')
+    }
+}
+
+ window.addEventListener('scroll', scrollAppear);*/
+
+  const textElement = $('.text-1-slide-init');
+const sectionElement = $('.desk-width');
+
+function test() {
+        textElement.css({
+            'opacity': '1',
+            'transform': 'translateY(-15px)'
+        });
+} 
+
+// $(window).scroll(function(){
+//     test();
+// }); 
+
+sectionElement.mouseenter(function(){
+    test();
+}) ;
+
+
+//Source code https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
+$('a[href*="#"]').on('click', function (e) {
+  e.preventDefault()
+
+  $('html, body').animate(
+    {
+      scrollTop: $($(this).attr('href')).offset().top,
+    },
+    800,
+    'linear'
+  )
 });
