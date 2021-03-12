aaca810b1566547d9985eef65d25cb3ec2dcaf99
Nicola2309 <nicola.spigaglia@outlook.it>
2021-03-12 03:09:29 +0000

    Inserted figcaption into figure element in recipe page

--- static/js/script.js
+++ static/js/script.js
@@ -13,36 +13,11 @@ $(document).ready(function(){
 
 });
 
-/* Scrolling navbar code inspired from the tutorial at this link https://www.youtube.com/watch?v=HINs4W_pGGU&ab_channel=ConorBailey */
+/* Scrolling navbar code inspired from the tutorial by Techno Solution at this link https://www.youtube.com/watch?v=HINs4W_pGGU&ab_channel=ConorBailey */
 
 $(window).scroll(function(){
 
     $('nav').toggleClass('scrolled', $(this).scrollTop()>42);
 });
 
-// Transition Functions 
-/*function scrollAppear(){
-    let textSlideInit = document.querySelector('.text-1-slide-init');
-    let beginPosition = textSlideInit.getBoundingClientRect().top;
-    let screenPosition = window.innerHeight / 1.5;
 
-    if(beginPosition < screenPosition){
-        textSlideInit.classList.add('.text-1-slide-end')
-    }
-}
-
- window.addEventListener('scroll', scrollAppear);*/
-
-
-//Source code https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
-/*$('a[href*="#"]').on('click', function (e) {
-  e.preventDefault()
-
-  $('html, body').animate(
-    {
-      scrollTop: $($(this).attr('href')).offset().top,
-    },
-    800,
-    'linear'
-  )
-});*/
